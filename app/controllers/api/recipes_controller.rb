98b68b4f3a41db71d73424aa346ae1538846eeae
dani <dani.zaghian@gmail.com>
2020-06-01 12:22:49 -0700

    adds restful index and show actions

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,8 +1,14 @@
 class Api::RecipesController < ApplicationController
 
-  def one_recipe_action
-    @recipe = Recipe.first
-    render "one_recipe.json.jb"
+  def index
+    @recipes = Recipe.all
+    render "index.json.jb"
+  end
+
+  def show
+    # .find method takes in just an id (as opposed to .find_by which takes in a key value pairing)
+    @recipe = Recipe.find(params[:id])
+    render "show.json.jb"
   end
 
 end
