101fcddbd761912fc3689d033619d17e4290a8f6
dani <dani.zaghian@gmail.com>
2020-06-17 10:47:08 -0700

    adds recipes new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   get "/recipes" => "recipes#index"
+  get "/recipes/new" => "recipes#new"
   get "/recipes/:id" => "recipes#show"
-
+ 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/recipes" => "recipes#index"
