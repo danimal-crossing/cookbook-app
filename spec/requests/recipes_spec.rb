9a40811430891cd32bca2979cf7958f12aec954c
dani <dani.zaghian@gmail.com>
2020-07-16 10:42:32 -0700

    adds recipe create tests

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -25,4 +25,60 @@ RSpec.describe "Recipes", type: :request do
       expect(recipe["title"]).to eq("Example Title 1")
     end
   end
+  describe "POST /recipes" do
+    it "should create a new recipe" do
+      user = User.first
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      post "/api/recipes", 
+      params: {
+        title: "New Title", 
+        ingredients: "New Ingredients",
+        directions: "New Directions",
+        prep_time: 20,
+        image_url: "New Image Url"
+      },
+      headers: { "Authorization" => "Bearer #{jwt}"}
+      recipe = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(recipe["title"]).to eq("New Title")
+    end
+    it 'returns an error status code with an unauthorized user' do
+      post "/api/recipes", 
+      params: {
+        title: "New Title", 
+        ingredients: "New Ingredients",
+        directions: "New Directions",
+        prep_time: 20,
+        image_url: "New Image Url"
+      }
+      expect(response).to have_http_status(401)
+    end
+    it 'returns an error status code with invalid data' do
+      user = User.first
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      post "/api/recipes", 
+      params: {
+        title: "", 
+        ingredients: "",
+        directions: "",
+        prep_time: 20,
+        image_url: ""
+      },
+      headers: { "Authorization" => "Bearer #{jwt}"}
+      recipe = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+    end
+  end
 end
